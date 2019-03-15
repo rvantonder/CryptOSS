438afeb5d2d7326167d50fe563ed0450b4756d28
Rijnard van Tonder <rvantonder@gmail.com>
2019-03-15 11:08:56 -0400

    rename to html

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 RELEASE_TARGET=docs
+DATE=`basename $1`
 
 # unzip
 cd $1
 tar xf dats.tar.gz
 cd ../..
 
-mkdir -p $RELEASE_TARGET/`basename $1`
+mkdir -p $RELEASE_TARGET/$DATE
 
 # create aggs
 ./crunch.exe show | xargs -L 1 -I % ./crunch.exe aggregate % -dir $1
@@ -16,9 +17,14 @@ mkdir -p $RELEASE_TARGET/`basename $1`
 # generate currency pages
 ./crunch.exe show | xargs -L 1 -I % ./site_html_generator.exe -dir $1 -cryptos % 
 
-mv *.html $RELEASE_TARGET/`basename $1`
-mv currency $RELEASE_TARGET/`basename $1`
-cp -r site/static $RELEASE_TARGET/`basename $1`/
+mv *.html $RELEASE_TARGET/$DATE
+mv currency $RELEASE_TARGET/$DATE
+
+cd $RELEASE_TARGET/$DATE/currency
+ls * | xargs -L 1 -I % mv %{,.html}
+cd ../../../
+
+cp -r site/static $RELEASE_TARGET/$DATE/
 
 # delete .dat, .agg
 cd $1
